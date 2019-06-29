232ce2cc40df8306beea41bba5b7c5203841832d
Emilio <pringles212@gmail.com>
2019-06-29 12:33:12 -0400

    añadiendo plataforma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { Phaser } from 'node_modules/phaser/dist/phaser';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
